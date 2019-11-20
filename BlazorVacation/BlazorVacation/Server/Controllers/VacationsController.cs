d87662a5fdf45316b02d888cb8bc81a09d157be7
Marko Lohert <mlohert@yahoo.com>
2019-11-21 00:01:18 +0100

    Add new vacation.

--- BlazorVacation/BlazorVacation/Server/Controllers/VacationsController.cs
+++ BlazorVacation/BlazorVacation/Server/Controllers/VacationsController.cs
@@ -15,5 +15,19 @@ namespace BlazorVacation.Server.Controllers
             var dal = new DalVacations();
             return dal.GetSubmittedVacationsCurrentUser(year);
         }
+
+        [HttpGet("[action]")]
+        public int GetTotalAssignedVacationDays(int year)
+        {
+            var dal = new DalVacations();
+            return dal.GetTotalAssignedVacationDays(year);
+        }
+
+        [HttpPut("[action]")]
+        public void AddNewVacation([FromBody]Vacation newVacation)
+        {
+            var dal = new DalVacations();
+            dal.AddNewVacation(newVacation);
+        }
     }
 }
---
+++ BlazorVacation/BlazorVacation/Server/DAL/DalHolidays.cs
@@ -0,0 +1,46 @@
+﻿using BlazorVacation.Shared;
+using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+
+namespace BlazorVacation.Server.DAL
+{
+    public class DalHolidays
+    {
+        public List<Holiday> GetHolidays(int year)
+        {
+            List<Holiday> listHoliday = new List<Holiday>();
+
+            string cmdText = $@"SELECT *
+                                FROM HOLIDAY
+                                WHERE YEAR(FROM_DATE) = {year} OR
+	                                  YEAR(TILL_DATE) = {year};";
+
+            using SqlConnection connection = new SqlConnection(ConnectionString);
+            connection.Open();
+            SqlCommand sqlCommand = new SqlCommand(cmdText, connection);
+
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+
+            while (reader.Read())
+            {
+                listHoliday.Add
+                (
+                    new Holiday
+                    {
+                        FromDate = (DateTime)reader["FROM_DATE"],
+                        TillDate = (DateTime)reader["TILL_DATE"],
+                        Name = (string)reader["Name"]
+                    }
+                );
+            }
+
+            reader.Close();
+            connection.Close();
+
+            return listHoliday;
+        }
+
+        private string ConnectionString = "Data Source = .;Initial Catalog = BlazorVacation;Integrated Security=true";
+    }
+}
