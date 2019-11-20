fab2578cc4201884bb98ac080f62aed1cbd53039
Marko Lohert <mlohert@yahoo.com>
2019-11-20 23:42:10 +0100

    List of already submitted vacations is displayed on My Vacations page.

---
+++ BlazorVacation/BlazorVacation/Server/Controllers/VacationsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using BlazorVacation.Server.Dal;
+using BlazorVacation.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorVacation.Server.Controllers
+{
+    [Route("api/[controller]")]
+    public class VacationsController : Controller
+    {
+        [HttpGet("[action]")]
+        public List<Vacation> GetSubmittedVacations(int year)
+        {
+            var dal = new DalVacations();
+            return dal.GetSubmittedVacationsCurrentUser(year);
+        }
+    }
+}
