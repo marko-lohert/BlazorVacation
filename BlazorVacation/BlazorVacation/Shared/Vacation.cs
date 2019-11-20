eab41cac764f67591887e0182ae40616e3e92770
Marko Lohert <mlohert@yahoo.com>
2019-11-21 00:23:07 +0100

    Forms and validation used to verify new vacation inputed from the user.

--- BlazorVacation/BlazorVacation/Shared/Vacation.cs
+++ BlazorVacation/BlazorVacation/Shared/Vacation.cs
@@ -1,14 +1,22 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace BlazorVacation.Shared
 {
     public class Vacation
     {
+        [Required]
         public DateTime FromDate { get; set; }
+        [Required]
         public DateTime TillDate { get; set; }
+        [Required]
+        [StringLength(maximumLength: 50, ErrorMessage="The maximum length of the note is 50 characters.")]
         public string? Note { get; set; }
+        [Required]
         public int Duration { get; set; }
+        [Required]
         public bool Approved { get; set; }
+        [Required]
         public bool SetUpOutOfOfficeEmail { get; set; }
     }
 }
