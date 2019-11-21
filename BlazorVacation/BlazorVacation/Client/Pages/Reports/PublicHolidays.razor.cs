e8e245aac425435fd86b9b48f0b2462208d879c7
Marko Lohert <mlohert@yahoo.com>
2019-11-21 02:25:11 +0100

    Use partial classes to separate presentation from logic.

---
+++ BlazorVacation/BlazorVacation/Client/Pages/Reports/PublicHolidays.razor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using BlazorVacation.Shared;
+
+namespace BlazorVacation.Client.Pages.Reports
+{
+    public partial class PublicHolidays
+    {
+        [Parameter]
+        public string? Year { get; set; } // string? instead of int because Year is used as parameter in routing.
+
+        List<Holiday>? Holidays;
+        static List<int> ListYears = Enumerable.Range(2019, 3).ToList<int>();
+        public string? SelectedYear { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            if (string.IsNullOrEmpty(Year))
+                Year = DateTime.Now.Year.ToString();
+
+            Holidays = await Http.GetJsonAsync<List<Holiday>>($"api/Holidays/GetHolidays?year={Year}");
+
+            SelectedYear = Year;
+        }
+
+        async Task ChangeYear()
+        {
+            Holidays = await Http.GetJsonAsync<List<Holiday>>($"api/Holidays/GetHolidays?year={Year}");
+            this.StateHasChanged();
+        }
+    }
+}
---
+++ BlazorVacation/BlazorVacation/Client/Pages/Vacations/AlradySubmittedVacations.razor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using BlazorVacation.Shared;
+
+namespace BlazorVacation.Client.Pages.Vacations
+{
+    public partial class AlradySubmittedVacations
+    {
+        List<Vacation>? Vacations;
+
+        protected override async Task OnInitializedAsync()
+        {
+            int thisYear = DateTime.Today.Year;
+            Vacations = await Http.GetJsonAsync<List<Vacation>>($"api/Vacations/GetSubmittedVacations?year={thisYear}");
+        }
+    }
+}
